450033aceb1f84259ee48028dc2282b1877b554a
Hritik Kumar Sharma <hritix@gmail.com>
2020-05-21 07:55:51 +0530

    About Us & Contact have been added :)

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Home from './HomeComponent';
+import AboutUs from './AboutComponent';
+import Contact from './ContactComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
+import {LEADERS} from '../shared/leaders';
 
 const MenuNavigator = createStackNavigator({
   Menu: { screen: Menu },
@@ -25,9 +28,36 @@ const MenuNavigator = createStackNavigator({
 }
 );
 
-
 const HomeNavigator = createStackNavigator({
   Home: { screen: Home }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
+
+const ContactNavigator = createStackNavigator({
+  Contact: { screen: Contact }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
+
+const AboutUsNavigator = createStackNavigator({
+  AboutUs: { screen: AboutUs }
 }, {
   navigationOptions: ({ navigation }) => ({
     headerStyle: {
@@ -54,7 +84,25 @@ const MainNavigator = createDrawerNavigator({
         title: 'Menu',
         drawerLabel: 'Menu'
       }, 
-    }
+    },
+  Contact:
+  {
+    screen: ContactNavigator,
+    navigationOptions: 
+    {
+      title: 'Contact',
+      drawerLabel: 'Contact'
+    },
+  },
+  AboutUs:
+  {
+    screen: AboutUsNavigator,
+    navigationOptions: 
+    {
+      title: 'About Us',
+      drawerLabel: 'About Us'
+    },
+  }
 }, {
 drawerBackgroundColor: '#D1C4E9'
 });
