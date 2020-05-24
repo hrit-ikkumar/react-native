8da984c778587b3a06381d613b688f419d06aa87
Hritik Kumar Sharma <hritix@gmail.com>
2020-05-24 08:55:05 +0530

    Assignment 2

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,105 +1,228 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
+import { View, Text, ScrollView, FlatList, Modal, Button, StyleSheet } from 'react-native';
+import { Card, Input, Icon, Rating } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-import { postFavorite } from '../redux/ActionCreators';
+import { postFavorite, postComment } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
-      dishes: state.dishes,
-      comments: state.comments,
-      favorites: state.favorites
+        dishes: state.dishes,
+        comments: state.comments,
+        favorites: state.favorites
     }
-  }
-  const mapDispatchToProps = dispatch => ({
-    postFavorite: (dishId) => dispatch(postFavorite(dishId))
+}
+
+const mapDispatchToProps = dispatch => ({
+    postFavorite: dishId => dispatch(postFavorite(dishId)),
+    postComment: (dishId, author, comment, rating) => dispatch(postComment(dishId, author, comment, rating))
 })
 
 function RenderComments(props) {
-
     const comments = props.comments;
-            
-    const renderCommentItem = ({item, index}) => {
-        
+    console.log("Comments: " + JSON.stringify(comments));
+
+    const renderCommentItem = ({ item, index }) => {
         return (
-            <View key={index} style={{margin: 10}}>
-                <Text style={{fontSize: 14}}>{item.comment}</Text>
-                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
-                <Text style={{fontSize: 12}}>{'-- ' + item.author + ', ' + item.date} </Text>
+            <View key={index} style={{ margin: 10 }}>
+                <Text style={{ fontSize: 14 }}>{item.comment}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>{'-- ' + item.author + ', ' + item.date}</Text>
             </View>
         );
-    };
-    
+    }
+
     return (
-        <Card title='Comments' >
-        <FlatList 
-            data={comments}
-            renderItem={renderCommentItem}
-            keyExtractor={item => item.id.toString()}
+        <Card title="Comments">
+            <FlatList
+                data={comments}
+                renderItem={renderCommentItem}
+                keyExtractor={item => item.dishId.toString()}
             />
         </Card>
-    );
+    )
 }
 
-function RenderDish(props) {
-
-    const dish = props.dish;
-    
-        if (dish != null) {
-            return(
-                <Card
-                featuredTitle={dish.name}
-                image={{uri: baseUrl + dish.image}}>
-                    <Text style={{margin: 10}}>
-                        {dish.description}
-                    </Text>
-                    <Icon
-                        raised
-                        reverse
-                        name={ props.favorite ? 'heart' : 'heart-o'}
-                        type='font-awesome'
-                        color='#f50'
-                        onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
-                        />
-                </Card>
-            );
-        }
-        else {
-            return(<View></View>);
-        }
-}
-
-class DishDetail extends Component {
-
+class Dishdetail extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            favorites: []
-        };
+            rating: '',
+            author: '',
+            comment: '',
+            date: '',
+            showModal: false
+        }
+    }
+
+    toggleModal() {
+        this.setState({ showModal: !this.state.showModal });
+    }
+
+    resetForm() {
+        this.setState({
+            rating: '',
+            author: '',
+            comment: '',
+            date: '',
+            showModal: false
+        });
+    }
+
+    handleCommentSubmit(dishId) {
+        console.log("In handleCommentSubmit");
+        console.log(JSON.stringify(this.state));
+        this.props.postComment(dishId, this.state.author, this.state.comment, this.state.rating);
+        this.toggleModal();
+        this.resetForm();
     }
 
     markFavorite(dishId) {
         this.props.postFavorite(dishId);
     }
-    
+
     static navigationOptions = {
-        title: 'Dish Details'
+        title: 'Dish Details',
+
     };
 
     render() {
-        const dishId = this.props.navigation.getParam('dishId','');
-        return(
+        const dishId = this.props.navigation.getParam('dishId', '');
+
+        const RenderDish = (props) => {
+            const dish = props.dish;
+            console.log("Dish: " + JSON.stringify(dish));
+
+            if (dish != null) {
+                console.log("try to render card");
+                console.log("Props: " + JSON.stringify(props));
+                return (
+                    <Card
+                        featuredTitle={dish.name}
+                        image={{ uri: baseUrl + dish.image }}
+                    >
+                        <Text style={{ margin: 10 }}>
+                            {dish.description}
+                        </Text>
+                        <View style={styles.formRow}>
+                        <Icon
+                            key={0}
+                            raised
+                            reversed
+                            name={props.favorite ? 'heart' : 'heart-o'}
+                            type='font-awesome'
+                            color='#f50'
+                            style={styles.formItem}
+                            onPress={() => props.favorite ? console.log('already favorite') : props.onPress()} />
+                        <Icon
+                            key={1}
+                            raised
+                            reversed
+                            reverseColor="512DA8"
+                            name={'pencil'}
+                            type='font-awesome'
+                            style={styles.formItem}
+                            color='#512DA8'
+                            onPress={() => { this.toggleModal() }} />
+                        </View>
+                    </Card>
+                );
+            } else {
+                console.log("Returning empty view");
+                return (
+                    <View></View>
+                );
+            }
+        }
+
+        console.log('dishId: ' + dishId);
+        return (
             <ScrollView>
-            <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                 favorite={this.props.favorites.some(el => el === dishId)}
-                onPress={() => this.markFavorite(dishId)} 
+                <RenderDish key={0} dish={this.props.dishes.dishes[+dishId]}
+                    favorite={this.props.favorites.some(elm => elm == dishId)}
+                    onPress={() => this.markFavorite(dishId)}
                 />
-            <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
-        </ScrollView>
+                        <Modal animationType={"slide"} transparent={false}
+                            visible={this.state.showModal}
+                            onDismiss={() => this.toggleModal()}
+                            onRequestClose={() => this.toggleModal()}>
+                            <View style={styles.modal}>
+
+                                <Rating showRating fractions="{1}" startingValue="{3.3}" onFinishRating={(rating) => {this.setState({rating:rating})}} />
+
+                                <Input
+                                    reversed
+                                    placeholder="author"
+                                    leftIcon={{ type: 'font-awesome', name: 'user' }}
+                                    style={styles}
+                                    onChangeText={(value) => {this.setState({ author: value })}}
+                                />
+
+                            
+                                <Input
+                                    reversed
+                                    placeholder="Comment"
+                                    leftIcon={{ type: 'font-awesome', name: 'comment' }}
+                                    style={styles}
+                                    onChangeText={(value) => {this.setState({ comment: value })}}
+                                />
+
+                                <View style={styles.formRow}>
+                                <Button
+                                    onPress={() => { this.handleCommentSubmit(dishId); }}
+                                    color="#512DA8"
+                                    title="Submit"
+                                    style={{marginBottom: 2, marginTop: 2}}
+                                />
+                                </View>
+                                <View style={styles.formRow}>
+                                <Button
+                                    onPress={() => { this.toggleModal(); this.resetForm(); }}
+                                    color="grey"
+                                    title="Close"
+                                    style={{marginBottom: 2, marginTop: 2}}
+                                />
+                                </View>
+                            </View>
+                        </Modal>                
+                <RenderComments key={1} comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
+            </ScrollView>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    formRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    formLabel: {
+        fontSize: 18,
+        flex: 2
+    },
+    formItem: {
+        flex: 1
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor: '#512DA8',
+        textAlign: 'center',
+        color: 'white',
+        marginBottom: 20
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10
+    }
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(DishDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -1,29 +1,29 @@
 import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
+import { comments } from './comments';
 
 export const fetchComments = () => (dispatch) => {
     return fetch(baseUrl + 'comments')
-    .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
-      error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-      })
-    .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)))
-    .catch(error => dispatch(commentsFailed(error.message)));
-};
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error: ' + response.status + ', ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errMess = new Error(error.messsage);
+            throw errMess;
+        })
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)))
+}
 
-export const commentsFailed = (errmess) => ({
+export const commentsFailed = (errMess) => ({
     type: ActionTypes.COMMENTS_FAILED,
-    payload: errmess
+    payload: errMess
 });
 
 export const addComments = (comments) => ({
@@ -32,35 +32,28 @@ export const addComments = (comments) => ({
 });
 
 export const fetchDishes = () => (dispatch) => {
-
     dispatch(dishesLoading());
-
     return fetch(baseUrl + 'dishes')
-    .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
-      error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-      })
-    .then(response => response.json())
-    .then(dishes => dispatch(addDishes(dishes)))
-    .catch(error => dispatch(dishesFailed(error.message)));
-};
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error: ' + response.status + ', ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errMess = new Error(error.messsage);
+            throw errMess;
+        })
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(commentsFailed(error.message)))
+}
 
-export const dishesLoading = () => ({
-    type: ActionTypes.DISHES_LOADING
-});
-
-export const dishesFailed = (errmess) => ({
+export const dishesFailed = (errMess) => ({
     type: ActionTypes.DISHES_FAILED,
-    payload: errmess
+    payload: errMess
 });
 
 export const addDishes = (dishes) => ({
@@ -68,36 +61,33 @@ export const addDishes = (dishes) => ({
     payload: dishes
 });
 
-export const fetchPromos = () => (dispatch) => {
-    
-    dispatch(promosLoading());
-
-    return fetch(baseUrl + 'promotions')
-    .then(response => {
-        if (response.ok) {
-            return response;
-        } else {
-            var error = new Error('Error ' + response.status + ': ' + response.statusText);
-            error.response = response;
-            throw error;
-        }
-        },
-        error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-        })
-    .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)))
-    .catch(error => dispatch(promosFailed(error.message)));
-};
-
-export const promosLoading = () => ({
-    type: ActionTypes.PROMOS_LOADING
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
 });
 
-export const promosFailed = (errmess) => ({
+export const fetchPromos = () => (dispatch) => {
+    dispatch(promosLoading());
+    return fetch(baseUrl + 'promotions')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error: ' + response.status + ', ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errMess = new Error(error.messsage);
+            throw errMess;
+        })
+        .then(response => response.json())
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)))
+}
+
+export const promosFailed = (errMess) => ({
     type: ActionTypes.PROMOS_FAILED,
-    payload: errmess
+    payload: errMess
 });
 
 export const addPromos = (promos) => ({
@@ -105,36 +95,33 @@ export const addPromos = (promos) => ({
     payload: promos
 });
 
-export const fetchLeaders = () => (dispatch) => {
-    
-    dispatch(leadersLoading());
-
-    return fetch(baseUrl + 'leaders')
-    .then(response => {
-        if (response.ok) {
-            return response;
-        } else {
-            var error = new Error('Error ' + response.status + ': ' + response.statusText);
-            error.response = response;
-            throw error;
-        }
-        },
-        error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-        })
-    .then(response => response.json())
-    .then(leaders => dispatch(addLeaders(leaders)))
-    .catch(error => dispatch(leadersFailed(error.message)));
-};
-
-export const leadersLoading = () => ({
-    type: ActionTypes.LEADERS_LOADING
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
 });
 
-export const leadersFailed = (errmess) => ({
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading());
+    return fetch(baseUrl + 'leaders')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error: ' + response.status + ', ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errMess = new Error(error.messsage);
+            throw errMess;
+        })
+        .then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .catch(error => dispatch(leadersFailed(error.message)))
+}
+
+export const leadersFailed = (errMess) => ({
     type: ActionTypes.LEADERS_FAILED,
-    payload: errmess
+    payload: errMess
 });
 
 export const addLeaders = (leaders) => ({
@@ -142,15 +129,71 @@ export const addLeaders = (leaders) => ({
     payload: leaders
 });
 
-export const postFavorite = (dishId)  => (dispatch) => {
+export const leadersLoading = () => ({
+    type: ActionTypes.LEADERS_LOADING
+});
 
+export const postFavorite = (dishId) => (dispatch) => {
     setTimeout(() => {
         dispatch(addFavorite(dishId));
     }, 2000);
-};
-
+}
 
 export const addFavorite = (dishId) => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: dishId
 });
+
+export const addComment = (comment) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
+
+export const postComment = (dishId, author, comment, rating) => (dispatch) => {
+    const newComment = {
+        dishId: dishId,
+        rating: rating,
+        author: author,
+        comment: comment
+    }
+    newComment.date = new Date().toISOString();
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+}
+
+export const postComment2 = (dishId, author, comment, rating) => (dispatch) => {
+    const newComment = {
+        dishId: dishId,
+        rating: rating,
+        author: author,
+        comment: comment
+    }
+    newComment.date = new Date().toISOString();
+    return fetch(baseUrl + 'comments', {
+        method: 'POST',
+        body: JSON.stringify(newComment),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+    })
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error: ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(response => dispatch(addComment(response)))
+        .catch(error => {
+            console.log('Post comments', error.message);
+            alert('Your comment could not be posted\nError: ' + error.message)
+        })
+}
