2511caca4814c4a4d504a1a2609ad66ce417db2b
Hritik Kumar Sharma <hritix@gmail.com>
2020-05-28 08:10:02 +0530

    Image Manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import { baseUrl } from '../shared/baseUrl';
 import * as Permissions from 'expo-permissions';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from "expo-image-manipulator";
 
 class LoginTab extends Component {
 
@@ -145,9 +146,22 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri });
+                this.processImage(capturedImage.uri);
             }
         }
+
+    }
+
+    processImage = async (imageUri) => {
+        let processedImage = await ImageManipulator.manipulate(
+            imageUri, 
+            [
+                {resize: {width: 400}}
+            ],
+            {format: 'png'}
+        );
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri });
 
     }
     
